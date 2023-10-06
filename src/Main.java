1c272b799721ebd82af0bb3aa7627f1cb58a5bba
iammrunu <124758020+iammrunu@users.noreply.github.com>
2023-10-05 23:14:12 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+            p1.Name="Rahul";
+        p2.Age=35
+        p1.Age=38
+        p3.Gender="Male"
+        p4.id=1234
+
+        p1.changeName();
+        p2.changeAge();
+        p4.changeid();
+
+    }
+
+
+}
